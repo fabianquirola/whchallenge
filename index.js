c96ebd567a60ef8b51bace418fe1d7d1832f8ea2
fabianquirola <fabianquirola@hotmail.com>
2020-10-26 08:45:59 -0500

    modulo

--- index.js
+++ index.js
@@ -13,12 +13,13 @@ function numPaths(warehouse) {
 
     const n = warehouse.length;
     const m = warehouse[0].length;
+    const M = 1000000007;
 
     let visitedWh = Array(n).fill().map(()=>Array(m).fill(false));
 
     count = counter(warehouse,0,0,visitedWh,count)
     
-    return count;
+    return count%M;
 }
 
 function counter(warehouses,x,y,visitedWh,count){
@@ -39,10 +40,16 @@ function counter(warehouses,x,y,visitedWh,count){
         console.log
         if(x+1 < n && !visitedWh[x+1][y]){
             count = counter(warehouses,x+1,y,visitedWh,count);
-        }
+        }/*
+        if(x-1 >= 0 && !visitedWh[x-1][y]){
+            count = counter(warehouses,x-1,y,visitedWh,count);
+        }*/
         if(y+1 < m && !visitedWh[x][y+1]){
             count = counter(warehouses,x,y+1,visitedWh,count);
-        }
+        }/*
+        if(y-1 >= 0 && !visitedWh[x][y-1]){
+            count = counter(warehouses,x,y-1,visitedWh,count);
+        }*/
     }
     
     visitedWh[x][y]= false;
